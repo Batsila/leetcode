a6855235c3a8c3467ba1cf293badf8ae2c137554
Arsienij Aniščanka <BatsilaBox3@gmail.com>
2021-04-01 12:53:02 +0300

    Add leetcode 234

---
+++ LeetCode/0234. Palindrome Linked List.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) 
+    {
+        if(!head)
+        {
+            return false;
+        }
+        
+        ListNode* s = head;
+        ListNode* f = head;
+        
+        while(f && f->next)
+        {
+            s = s->next;
+            f = f->next->next;
+        }
+        
+        ListNode* m;
+        
+        if(f)
+        {
+            m = s->next;
+        }
+        else
+        {
+            m = s;
+        }
+        
+        ListNode* c = m;
+        ListNode* n;
+        ListNode* p = NULL;
+        
+        while(c)
+        {
+            n = c->next;
+            c->next = p;
+            p = c;
+            c = n;
+        }
+        
+        while(p)
+        {
+            if(head->val != p->val)
+            {
+                return false;
+            }
+            
+            head= head->next;
+            p = p->next;
+        }
+        
+        return true;  
+    }
+};
