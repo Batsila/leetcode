36273aaff8be7fa3b80294f8e32f24864bfaaae0
Arsienij Aniščanka <BatsilaBox3@gmail.com>
2021-04-04 23:41:01 +0300

    Add leetcode 622

---
+++ LeetCode/0622. Design Circular Queue.cpp
@@ -0,0 +1,73 @@
+class MyCircularQueue 
+{
+private:
+    vector<int> v;
+    int head = 0;
+    int tail = 0;
+    int capacity;
+public:
+    MyCircularQueue(int k) 
+    {
+        v = vector<int>(k, -1);
+        capacity = k;
+    }
+    
+    bool enQueue(int value) 
+    {
+        if (isFull())
+        {
+            return false;
+        }
+        
+        v[tail % capacity] = value;
+        
+        ++tail;
+        
+        return true;
+    }
+    
+    bool deQueue() 
+    {
+        if (isEmpty())
+        {
+            return false;
+        }
+        
+        v[head % capacity] = -1;
+        
+        ++head;
+        
+        return true;
+    }
+    
+    int Front() 
+    {
+        return v[head % capacity];
+    }
+    
+    int Rear() 
+    {
+        return v[(tail - 1) % capacity];
+    }
+    
+    bool isEmpty() 
+    {
+        return head == tail;
+    }
+    
+    bool isFull() 
+    {
+        return tail - head == capacity;
+    }
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
