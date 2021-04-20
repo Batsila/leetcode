b44eccc3cfd1d0923f36f62f7dcbb3a2efb7f5b8
Arsienij Aniščanka <BatsilaBox3@gmail.com>
2021-04-20 13:05:19 +0300

    Add leetcode 589

---
+++ LeetCode/0589. N-ary Tree Preorder Traversal.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution 
+{
+    vector<int> ans;
+public:
+    vector<int> preorder(Node* root) 
+    {
+        dfs(root);
+        
+        return ans;
+    }
+    
+    void dfs(Node* node)
+    {
+        if (node)
+        {
+            ans.push_back(node->val);
+            
+            for (auto c : node->children)
+            {
+               dfs(c); 
+            }
+        }
+    }
+};
