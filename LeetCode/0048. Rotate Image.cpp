d1237b4a2b9b1bb1dcf462ceffbd6708620d1624
Arsienij Aniščanka <BatsilaBox3@gmail.com>
2021-04-25 19:31:18 +0300

    Add leetcode 48

---
+++ LeetCode/0048. Rotate Image.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+public:
+    void rotate(vector<vector<int>>& matrix) 
+    {
+        for (int i = 0; i < matrix.size(); ++i)
+        {
+            for (int j = i; j < matrix.size(); ++j)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        for (int i = 0; i < matrix.size(); ++i)
+        {
+            for (int j = 0; j < matrix.size() / 2; ++j)
+            {
+                swap(matrix[i][j], matrix[i][matrix.size() - j - 1]);
+            }
+        }
+    }
+};
