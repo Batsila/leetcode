e0bff0a25788fd435f49ef03c81b0fc526bc3052
Arsienij Aniščanka <BatsilaBox3@gmail.com>
2021-05-26 10:34:06 +0300

    Add leetcode 1689

---
+++ LeetCode/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int minPartitions(string n)
+    {
+        int ans = 0;
+        
+        for (char c : n)
+        {
+            ans = max(ans, c - '0');
+        }
+        
+        return ans;
+    }
+};
