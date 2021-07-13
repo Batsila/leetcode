06f73b642b673b4f817a64c11c5248612e13ac13
Arsienij Aniščanka <batsilabox3@gmail.com>
2021-07-13 11:42:54 +0300

    Add 162

---
+++ algorithms/0162. Find Peak Element.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    int findPeakElement(vector<int>& nums)
+    {
+        int l = 0;
+        int r = nums.size() - 1;
+        
+        while (l < r)
+        {
+            int m = (l + r) / 2;
+            
+            if (nums[m] > nums[m + 1])
+            {
+                r = m;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+        
+        return l;
+    }
+};
