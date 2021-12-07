1fb627c8e66018d217ebadc62e84fca472145eee
Arsienij Aniščanka <batsilabox3@gmail.com>
2021-12-07 11:19:43 +0300

    Add 1290

---
+++ algorithms/1290. Convert Binary Number in a Linked List to Integer.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    int getDecimalValue(ListNode* head)
+    {
+        int ans = 0;
+        ListNode* c = head;
+        
+        while (c)
+        {
+            ans <<= 1;
+            ans |= c->val;
+            c = c->next;
+        }
+        
+        return ans;
+    }
+};
