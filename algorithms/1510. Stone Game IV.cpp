1cf16362a32f1f6dad8f022b2a2b9685874e4e04
Arsienij Aniščanka <batsilabox3@gmail.com>
2022-01-23 00:46:06 +0300

    Add 1510

---
+++ algorithms/1510. Stone Game IV.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    bool winnerSquareGame(int n)
+    {
+        vector<bool> dp(n + 1, false);
+        
+        for (int i = 0; i <= n; ++i)
+        {
+            if (not dp[i])
+            {
+                for (int j = 1; i + j * j <= n; ++j)
+                {
+                    dp[i + j * j] = true;
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+};
