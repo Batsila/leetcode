b6e6486395c5b612353fa754bb5541482e4bf787
Arsienij Aniščanka <batsilabox3@gmail.com>
2022-02-10 11:53:53 +0300

    Add 560

---
+++ algorithms/0560. Subarray Sum Equals K.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    int subarraySum(vector<int>& nums, int k)
+    {
+        int ans = 0;
+        unordered_map<int, int> m;
+        m[0] = 1;
+        
+        int sum = 0;
+        
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            sum += nums[i];
+            
+            if (m.find(sum - k) != m.end())
+            {
+                ans += m[sum - k];
+            }
+            
+            ++m[sum];
+        }
+        
+        return ans;
+    }
+};
