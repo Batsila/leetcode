16da666ca8a5355559fe1ad71d5a64206e57a5a5
Arsienij Aniščanka <batsilabox3@gmail.com>
2022-03-26 11:27:14 +0100

    Add 704

---
+++ algorithms/0704. Binary Search.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int search(vector<int>& nums, int target)
+    {
+        int l = 0;
+        int r = nums.size() - 1;
+        
+        while (l <= r)
+        {
+            int m = (l + r) / 2;
+            
+            if (nums[m] == target)
+            {
+                return m; 
+            }
+            
+            if (target < nums[m])
+            {
+                r = m - 1;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+        
+        return -1;
+    }
+};
