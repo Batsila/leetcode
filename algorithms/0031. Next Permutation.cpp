77591269dead5ba9b46f4f0bee091798d5dfe445
Arsienij Aniščanka <batsilabox3@gmail.com>
2022-04-03 22:14:30 +0200

    Add 31

---
+++ algorithms/0031. Next Permutation.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    void nextPermutation(vector<int>& nums)
+    {
+        int i = nums.size() - 2;
+        
+        while (i >= 0 and nums[i + 1] <= nums[i])
+        {
+            --i;
+        }
+        
+        if (i >= 0)
+        {
+            int j = nums.size() - 1;
+            
+            while (nums[j] <= nums[i])
+            {
+                --j;
+            }
+            
+            swap(nums[i], nums[j]);
+        }
+        
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
