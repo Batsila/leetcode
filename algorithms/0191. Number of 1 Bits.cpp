1689d94ead0f20e82896a8eeda084021e07f1d3a
Arsienij Aniščanka <batsilabox3@gmail.com>
2022-05-26 11:00:38 +0300

    Add 191

---
+++ algorithms/0191. Number of 1 Bits.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int hammingWeight(uint32_t n)
+    {
+        int c = 0;
+        
+        for (int i = 0; i < 32; ++i)
+        {
+            if (n >> i & 1)
+            {
+                ++c;
+            }
+        }
+        
+        return c;
+    }
+};
