c388cd69efb44ce323aab94bf881c84575858f26
Arsienij Aniščanka <batsilabox3@gmail.com>
2022-06-14 11:28:38 +0300

    Update 583

--- algorithms/0583. Delete Operation for Two Strings.cpp
+++ algorithms/0583. Delete Operation for Two Strings.cpp
@@ -1,18 +1,19 @@
 class Solution 
 {
-    int dp[555][555];
 public:
     int minDistance(string word1, string word2) 
-    {     
+    {
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1));
+        
         for (int i = 0; i <= word1.size(); ++i)
         {
             for (int j = 0; j <= word2.size(); ++j)
             {
-                if (!i)
+                if (i == 0)
                 {
                     dp[i][j] = j;
                 }
-                else if (!j)
+                else if (j == 0)
                 {
                     dp[i][j] = i;
                 }
