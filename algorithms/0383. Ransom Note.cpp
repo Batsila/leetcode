2087caa57b35bf41a958e0e175339961b235146a
Arsienij Aniščanka <batsilabox3@gmail.com>
2022-08-25 10:05:45 +0200

    Add 383

---
+++ algorithms/0383. Ransom Note.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    bool canConstruct(string ransomNote, string magazine)
+    {
+        map<char, int> m;
+        
+        for (char c : magazine)
+        {
+            ++m[c];
+        }
+        
+        for (char c : ransomNote)
+        {
+            if (m[c] > 0)
+            {
+                --m[c];
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
