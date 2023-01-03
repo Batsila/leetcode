01fcae11a71280efb2e1173b24166b072de51000
Arsienij Aniščanka <batsilabox3@gmail.com>
2023-01-03 14:23:56 +0100

    Add 944

---
+++ 0944. Delete Columns to Make Sorted.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int minDeletionSize(vector<string>& strs)
+    {
+        int ans = 0;
+
+        for (int i = 0; i < strs[0].size(); ++i)
+        {
+            for (int j = 1; j < strs.size(); ++j)
+            {
+                if (strs[j - 1][i] > strs[j][i])
+                {
+                    ++ans;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
