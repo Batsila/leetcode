5b36fd89bc64533d598cf21df310fcbe8dcfef9b
Arsienij Aniščanka <batsilabox3@gmail.com>
2023-02-17 09:33:18 +0100

    Add 530 and 783

---
+++ 0783. Minimum Distance Between BST Nodes.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+    vector<int> vals;
+public:
+    int minDiffInBST(TreeNode* root)
+    {
+        dfs(root);
+
+        int ans = INT_MAX;
+
+        for (int i = 1; i < vals.size(); ++i)
+        {
+            ans = min(ans, vals[i] - vals[i - 1]);
+        }
+
+        return ans;
+    }
+
+    void dfs(TreeNode* node)
+    {
+        if (node)
+        {
+            dfs(node->left);
+            vals.push_back(node->val);
+            dfs(node->right);
+        }
+    }
+};
