f8e838f81a3980e12423f7caaf10c856df117ec0
Arsienij Aniščanka <batsilabox3@gmail.com>
2023-12-20 11:50:46 +0100

    Add 2706

---
+++ 2706. Buy Two Chocolates.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int buyChoco(vector<int>& prices, int money)
+    {
+        sort(prices.begin(), prices.end());
+
+        if (prices[0] + prices[1] > money)
+        {
+            return money;
+        }
+        else
+        {
+            return money - prices[0] - prices[1];
+        }
+    }
+};
