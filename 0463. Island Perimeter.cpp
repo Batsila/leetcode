a2649570fc6db28212bcf448ca309fea35d41370
Arsienij Aniščanka <batsilabox3@gmail.com>
2024-04-18 10:23:38 +0200

    Update 463

--- 0463. Island Perimeter.cpp
+++ 0463. Island Perimeter.cpp
@@ -1,49 +1,34 @@
 class Solution
 {
-    vector<vector<int>> g;
     int dx[4] = { 0, 1, 0, -1 };
     int dy[4] = { 1, 0, -1, 0 };
 public:
     int islandPerimeter(vector<vector<int>>& grid)
     {
-        g = grid;
         int ans = 0;
-        
-        for (int i = 0; i < g.size(); ++i)
+
+        for (int i = 0; i < grid.size(); ++i)
         {
-            for (int j = 0; j < g[0].size(); ++j)
+            for (int j = 0; j < grid[i].size(); ++j)
             {
-                ans += foo(i, j);
+                if (grid[i][j] == 1)
+                {
+                    for (int k = 0; k < 4; ++k)
+                    {
+                        int x = i + dx[k];
+                        int y = j + dy[k];
+
+                        if (x < 0 or x >= grid.size()
+                            or y < 0 or y >= grid[i].size()
+                            or grid[x][y] == 0)
+                        {
+                            ++ans;
+                        }
+                    }
+                }
             }
         }
-        
+
         return ans;
-    }
-    
-    int foo(int i, int j)
-    {
-        if (g[i][j])
-        {
-            int p = 0;
-            
-            for (int k = 0; k < 4; ++k)
-            {
-                int x = i + dx[k];
-                int y = j + dy[k];
-                
-                if (x >= 0 and x < g.size() and y >= 0 and y < g[0].size())
-                {
-                    p += g[x][y] ? 0 : 1;
-                }
-                else
-                {
-                    ++p;
-                }
-            }
-            
-            return p;
-        }
-        
-        return 0;
     }
 };
