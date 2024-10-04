81899f2538ba62d158a298d228758cdb9823d738
Arsienij Aniščanka <batsilabox3@gmail.com>
2024-10-04 12:52:35 +0200

    Add 122 cs version

---
+++ 0122. Best Time to Buy and Sell Stock II.cs
@@ -0,0 +1,17 @@
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        var profit = 0;
+
+        for (int i = 1; i < prices.Length; ++i)
+        {
+            if (prices[i] > prices[i - 1])
+            {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+}
